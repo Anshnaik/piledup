3018ee94f361246c43eedf37e1f75ffb243a4547
Ansh Naik <nansh5841@gmail.com>
2025-04-01 18:04:12 +0530

    added dont know what

--- backend/route.js
+++ backend/route.js
@@ -1,9 +1,18 @@
 const express=require("express");
 const app=express();
 
+const path = require('path');
+
 app.set("view engine","ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '..', 'views'));
+
+app.use(express.static(path.join(__dirname,'..','public')));
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+});
+
 
 app.get("/home",(req,res)=>{
     res.render("home");
